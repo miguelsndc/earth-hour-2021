af02e4e79c759c3434eceb51272de80b7e36e5da
Miguel Nogueira <mglsndc@gmail.com>
2021-04-27 15:40:24 -0300

    change fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;600;700&display=swap"
             rel="stylesheet"
           />
         </Head>
