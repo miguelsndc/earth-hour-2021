79bcae278610ebdc12bd423f3548413c7da20f9e
miguelsndc <mglsndc@gmail.com>
2021-12-08 16:13:39 -0300

    fix: add alt to footer img

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -6,7 +6,12 @@ export const PageFooter = () => {
   return (
     <footer className={styles.container}>
       <div>
-        <Image src='/icons/instagram.svg' width={20} height={20} />
+        <Image
+          src='/icons/instagram.svg'
+          width={20}
+          height={20}
+          alt='Instagram logo'
+        />
         <span>@bio_ambiental</span>
       </div>
     </footer>
